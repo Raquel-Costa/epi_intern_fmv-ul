5b8e8d0930711608baa8b883a683ca65d751b159
Raquel Costa <raqueldelobocosta@gmail.com>
2020-09-24 14:37:40 +0100

    Passos para guardar ficheiros pelo R

--- Commit pelo R.R
+++ Commit pelo R.R
@@ -1,6 +1,6 @@
 #Guardar ficheiros pelo R diretamente:
 
   #1. Guardar ficheiro no pc na pasta que está ligada ao Github que neste caso é a prep-estagio-epi
-  #2. Fazer commit fo ficheiro, fazendo descrição o ficheiro no espaço em branco e carregando em commit
+  #2. Fazer commit do ficheiro, fazendo descrição o ficheiro no espaço em branco e carregando em commit
   #3. Fazer pull se o R não estiver com todos os ficheiros atualizados do github
   #4. Fazer push para o ficheiro que queremos ficar guardado no github
